7ec3306b65a31f3cd0cc14499460f384bdb89d90
Paulina Scarlata <ps15e@my.fsu.edu>
2019-03-12 13:42:34 -0400

    Added comments

--- tickers.py
+++ tickers.py
@@ -13,13 +13,13 @@ def save_tickers(n):
     uses beautifulsoup to organize pulled data
     searches for <a></a> with parent <h3> which indicates /symbol
     '''
-    fopen=open(sys.argv[2],'w+')
-    page = requests.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download')
-    soup = BeautifulSoup(page.text,"html.parser")
-    for link in soup.find_all('a'):
-        if(link.find_parent("h3")):
-            tik = link.get('href')
-            print(tik[30:].upper(),file=fopen)
+    fopen=open(sys.argv[2],'w+') #open the file in write mode
+    page = requests.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download') #saves all url info
+    soup = BeautifulSoup(page.text,"html.parser") #organizes all data from url for easy access
+    for link in soup.find_all('a'): #finds all headers that start with <a>
+        if(link.find_parent("h3")): #finds the a with h3 as the parent
+            tik = link.get('href') #grabs the value of href
+            print(tik[30:].upper(),file=fopen) #print only the ticker symbol to the file
     fopen.close()
 if __name__ ==  "__main__":
     save_tickers(int(sys.argv[1]))
