644b288ffd9c31c5c2adc8a73200939528f38271
Paulina Scarlata <ps15e@my.fsu.edu>
2019-03-21 17:48:03 -0400

    Started from scratch

--- tickers.py
+++ tickers.py
@@ -5,21 +5,41 @@
 import requests
 import sys
 import time
-import urllib.request
-from bs4 import BeautifulSoup
+from iex import Stock
+import lxml.html
+from  lxml import html
 
 def save_tickers(n):
     '''
-    uses beautifulsoup to organize pulled data
-    searches for <a></a> with parent <h3> which indicates /symbol
+    Args:
+        n: number of tickers to print to file
     '''
     fopen=open(sys.argv[2],'w+') #open the file in write mode
     page = requests.get('http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQrender=download') #saves all url info
-    soup = BeautifulSoup(page.text,"html.parser") #organizes all data from url for easy access
-    for link in soup.find_all('a'): #finds all headers that start with <a>
-        if(link.find_parent("h3")): #finds the a with h3 as the parent
-            tik = link.get('href') #grabs the value of href
-            print(tik[30:].upper(),file=fopen) #print only the ticker symbol to the file
+    doc = lxml.html.fromstring(page.content)
+    table = doc.xpath('//div[@class = "genTable thin"]')[0]
+    h3 = table.xpath('.//h3')
+    ticks = h3.xpath('.//a[@href]/text()')
+    total_ticks =[]
+    for tick in ticks:
+        total_ticks.append(ticks.text_content())
+    print(total_ticks)
+    #for i in  range(0,n+1):
+    #    print(ticks[i])
     fopen.close()
+
+def check_if_valid(ticker):
+    '''
+    uses iex-api-python to check if ticker has valid price function
+    Args:
+        ticker: fetched ticker from NASDAQ website
+    '''
+    price = Stock(ticker).price()
+    #print(price)
+    if price: #how to know if price function didnt work?
+        return True
+    else:
+        return False
+
 if __name__ ==  "__main__":
     save_tickers(int(sys.argv[1]))
