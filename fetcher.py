fde025c77318b3cf395e4394ed6fcd6ad2cd46d7
Max <maxcastanedaiv@gmail.com>
2019-03-26 19:34:00 -0400

    fetcher changes
    Edited printout, implemented time feature

--- fetcher.py
+++ fetcher.py
@@ -7,9 +7,16 @@ import sys,os
 from iex import Stock
 import datetime
 
-def update_stock_info(ticker):
+def addSecs(tm, secs):
+    date=datetime.datetime(100, 1, 1, tm.hour, tm.minute, tm.second)
+    date=date+datetime.timedelta(seconds=secs)
+    return date.time()
+
+def update_stock_info(ticker, stop):
     now=datetime.datetime.now()
 
+    if(now.time()>stop):
+        sys.exit()
     sys.stdout = open(os.devnull,"w")
     print(now.strftime("%H:%M"), file=outfile, end=',')
     print(ticker,file = outfile,end =',')
@@ -23,7 +30,12 @@ def update_stock_info(ticker):
 
 if __name__ == "__main__":
     outfile = open(sys.argv[3],"w")
+    present=datetime.datetime.now()
+    stopTime=addSecs(present, int(sys.argv[1]))
+    print(present)
+    print (stopTime)
+    print("Time, Ticker, latestPrice, latestVolume, Close, Open, low, high", file=outfile)
     with open(sys.argv[2],"r") as f:
         for tick in f:
-            update_stock_info(tick.strip())
+            update_stock_info(tick.strip(), stopTime)
     outfile.close()
