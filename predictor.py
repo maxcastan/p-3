f3ff94cf066dec4d536212312060b3342ee53c3c
Max <maxcastanedaiv@gmail.com>
2019-03-28 10:41:45 -0400

    predictor start
    pulled in data from csv file

--- predictor.py
+++ predictor.py
@@ -1,13 +1,27 @@
 import sys
 import csv
+import numpy as np
+from datetime import datetime
+import matplotlib.pyplot as plt
+'exec(%matplotlib inline)'
+
+
+
 def predictor(ticker,infofile,graphfile,col,t):
+    times=[]
+    value=[]
     with open(infofile) as csvfile:
         csvreader =  csv.reader(csvfile,delimiter=',')
         for row in csvreader:
             if row[1]==ticker:
-                latestPrice = row[2]
-                latestVolume = row[3]
-    print(latestPrice,latestVolume)
+                times.append(datetime.strptime(row[0], "%H:%M"))
+                if(col=='latestPrice'):
+                    value.append(float(row[2]))
+                else:
+                    value.append(float(row[3]))
+    print(value)
+    print(times)
+
 
 
 
