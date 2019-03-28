c028a988d8a2ccc1899aaf871adead6e35dd2beb
Max <maxcastanedaiv@gmail.com>
2019-03-28 12:24:55 -0400

    Predictor.py
    Regression training

--- predictor.py
+++ predictor.py
@@ -1,32 +1,41 @@
 import sys
 import csv
+import pandas as pd
 import numpy as np
 import pandas as pd
 from datetime import datetime
-from sklearn import datasets, linear_model
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 'exec(%matplotlib inline)'
 
 
 
 def predictor(ticker,infofile,graphfile,col,t):
-    times=[]
-    value=[]
-    dataset=np.loadtxt(infofile,delimiter=",")
-    print(dataset)
-    '''with open(infofile) as csvfile:
-        csvreader =  csv.reader(csvfile,delimiter=',')
-        for row in csvreader:
-            if row[1]==ticker:
-                times.append(datetime.strptime(row[0], "%H:%M"))
-                if(col=='latestPrice'):
-                    value.append(float(row[2]))
-                else:
-                    value.append(float(row[3]))
-    '''
-    #print(csvreader.shape)
-    #print(times.shape)
 
+    dataset=pd.read_csv(infofile)
+    df=dataset.loc[dataset[' Ticker']==ticker]
+    print(df)
+    newCol=' '+col
+    X=df[[newCol]]
+    Y=df['Time'].str.split(':').apply(lambda x: int(x[0])*60+int(x[1]))
+
+    print(X)
+    print(Y)
+
+    X_train, X_test, y_train, y_test=train_test_split(X, Y, test_size=0.2, random_state=0)
+
+    regressor=LinearRegression()
+    regressor.fit(X_train, y_train)
+    yprediction=regressor.predict(X_test)
+
+
+    plot.scatter(X_train, y_train, color= 'purple')
+    plot.plot(X_train, regressor.predict(X_train), color='green')
+    plot.title('Time vs '+col)
+    plot.xlabel(col)
+    plot.ylabel('Time')
+    plot.show()
 
 
 
