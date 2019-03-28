2f7707c3e8b2b5ccef54ac6a652cb4d2f69f817e
Max <maxcastanedaiv@gmail.com>
2019-03-28 13:49:38 -0400

    Predictor Changes
    The graph is now created with Time on the X axis. Also,
    the predicted time is now calculated

--- predictor.py
+++ predictor.py
@@ -1,8 +1,20 @@
+'''
+The command to run this program is:
+python3 predictor.py ticker info.csv graphfile.png latestPrice/Volume minutes
+
+
+Authors: Max Castaneda and Paulina Scarlata
+
+
+This module takes a ticker name, csv file, a graph filename, a selection of latestPrice/Volume and the next
+t minutes. The module then creates a plot of actual data as well as a plot of predicted data and
+plots the regresssion line onto a graph and exports the graph to the passed filename
+
+'''
 import sys
 import csv
 import pandas as pd
 import numpy as np
-import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plot
 from sklearn.model_selection import train_test_split
@@ -13,15 +25,22 @@ from sklearn.linear_model import LinearRegression
 
 def predictor(ticker,infofile,graphfile,col,t):
 
+    now=datetime.now()
+    timeMinutes=int(now.hour)*60+int(now.minute)
+    predictTime=timeMinutes+int(t)
+
+    new=[]
+    for x in range(timeMinutes, predictTime):
+        new.append(x)
+
+    print(new)
+
     dataset=pd.read_csv(infofile)
     df=dataset.loc[dataset[' Ticker']==ticker]
-    print(df)
     newCol=' '+col
-    Y=df[newCol]
-    X=df[df['Time'].str.split(':').apply(lambda x: int(x[0])*60+int(x[1]))]]
+    Y=df[[newCol]]
+    X=df['Time'].str.split(':').apply([lambda x: int(x[0])*60+int(x[1])])
 
-    print(X)
-    print(Y)
 
     X_train, X_test, y_train, y_test=train_test_split(X, Y, test_size=1/3, random_state=0)
 
@@ -32,10 +51,10 @@ def predictor(ticker,infofile,graphfile,col,t):
 
     plot.scatter(X_train, y_train, color= 'purple')
     plot.plot(X_train, regressor.predict(X_train), color='green')
-    plot.title('Time vs '+col)
-    plot.xlabel(col)
-    plot.ylabel('Time')
-    plot.show()
+    plot.title(col+' vs Time')
+    plot.ylabel(col)
+    plot.xlabel('Time')
+    plot.savefig(graphfile)
 
 
 
